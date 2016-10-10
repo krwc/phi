5bc0631b56cae3ec3d5b11a8fc510501428e1db2
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2016-10-10 17:43:03 +0200

    Implemented freelook camera

--- src/math/Math.h
+++ src/math/Math.h
@@ -2,5 +2,8 @@
 #define PHI_MATH_H
 
 #include "libs/glm/glm/glm.hpp"
+#include "libs/glm/glm/gtc/quaternion.hpp"
+#include "libs/glm/glm/gtc/type_ptr.hpp"
+#include "libs/glm/glm/gtc/matrix_transform.hpp"
 
 #endif
