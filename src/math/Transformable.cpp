c8849f140d5ab11cc69221eaa3dd84e5b91131f8
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2016-10-17 18:30:12 +0200

    Added possibility to perform scaling

--- src/math/Transformable.cpp
+++ src/math/Transformable.cpp
@@ -3,6 +3,8 @@
 namespace phi {
 using namespace glm;
 
+Transformable::Transformable() : m_diagonal(1, 1, 1) {}
+
 const mat4 &Transformable::GetTransform() const {
     return m_transform;
 }
@@ -13,6 +15,12 @@ vec3 Transformable::GetPosition() const {
 
 mat3 Transformable::GetRotation() const {
     return mat3(m_transform);
+}
+
+void Transformable::SetScale(const vec3 &s) {
+    m_transform[0][0] = m_diagonal[0] * s[0];
+    m_transform[1][1] = m_diagonal[1] * s[1];
+    m_transform[2][2] = m_diagonal[1] * s[2];
 }
 
 void Transformable::SetPosition(const vec3 &p) {
@@ -28,6 +36,9 @@ void Transformable::SetRotation(const vec3 &degrees) {
     mat3 R = mat3_cast(x*y*z);
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
+            if (i == j) {
+                m_diagonal[i] = R[i][j];
+            }
             m_transform[i][j] = R[i][j];
         }
     }
--- src/math/Transformable.h
+++ src/math/Transformable.h
@@ -6,11 +6,16 @@ namespace phi {
 
 class Transformable {
     glm::mat4 m_transform;
+    glm::vec3 m_diagonal;
+    bool m_scale_changed;
 
 public:
+    Transformable();
+    virtual ~Transformable() {}
     virtual const glm::mat4 &GetTransform() const;
     virtual glm::mat3 GetRotation() const;
     virtual glm::vec3 GetPosition() const;
+    virtual void SetScale(const glm::vec3 &s);
     virtual void SetPosition(const glm::vec3 &p);
     virtual void SetRotation(const glm::vec3 &degrees);
 };
