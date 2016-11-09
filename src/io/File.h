14a1b6cf5a1cc56270edbf6e11274483a377398d
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2016-11-09 21:14:09 +0100

    Introduced naive file loader

---
+++ src/io/File.h
@@ -0,0 +1,13 @@
+#ifndef PHI_IO_FILE_H
+#define PHI_IO_FILE_H
+#include <string>
+
+namespace phi {
+namespace io {
+
+std::string FileContents(const std::string &path);
+
+} // namespace io
+} // namespace phi
+
+#endif
