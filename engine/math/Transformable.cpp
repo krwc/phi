df7830bb0fc6606b22308c43f0b5711da7f8c1ff
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2016-11-20 17:44:27 +0100

    Fixed Transformable class again.

--- engine/math/Transformable.cpp
+++ engine/math/Transformable.cpp
@@ -3,45 +3,64 @@
 namespace phi {
 using namespace glm;
 
-Transformable::Transformable() {}
+Transformable::Transformable()
+        : m_rotation_angles(0, 0, 0), m_scale(1, 1, 1), m_position(0, 0, 0) {}
 
 const mat4 &Transformable::GetTransform() const {
     if (m_needs_update) {
         m_needs_update = false;
-        mat3 SR = m_rotation * m_scale;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                m_transform[i][j] = SR[i][j];
-            }
-        }
+        mat4 T = translate(mat4(), m_position);
+        mat4 R = m_rotation;
+        mat4 S = scale(mat4(), m_scale);
+        m_transform = T * R * S;
     }
     return m_transform;
 }
 
-vec3 Transformable::GetPosition() const {
-    return vec3(m_transform[3]);
+const vec3 &Transformable::GetPosition() const {
+    return m_position;
 }
 
 const mat3 &Transformable::GetRotation() const {
     return m_rotation;
 }
 
+const vec3 &Transformable::GetRotationAngles() const {
+    return m_rotation_angles;
+}
+
+const vec3 &Transformable::GetScale() const {
+    return m_scale;
+}
+
 void Transformable::SetScale(const vec3 &s) {
-    m_scale = mat3(scale(mat4(), s));
+    m_scale = s;
     m_needs_update = true;
 }
 
 void Transformable::SetPosition(const vec3 &p) {
-    m_transform[3][0] = p[0];
-    m_transform[3][1] = p[1];
-    m_transform[3][2] = p[2];
+    m_position = p;
+    m_needs_update = true;
 }
 
 void Transformable::SetRotation(const vec3 &degrees) {
-    quat x = rotate(quat(), radians(degrees.x), { 1, 0, 0 });
-    quat y = rotate(quat(), radians(degrees.y), { 0, 1, 0 });
-    quat z = rotate(quat(), radians(degrees.z), { 0, 0,-1 });
+    quat x = inverse(rotate(quat(), radians(degrees.x), { 1, 0, 0 }));
+    quat y = inverse(rotate(quat(), radians(degrees.y), { 0, 1, 0 }));
+    quat z = inverse(rotate(quat(), radians(degrees.z), { 0, 0,-1 }));
     m_rotation = mat3_cast(x*y*z);
+#warning "TODO: this can be done without storing rotation angles"
+    m_rotation_angles.x = fmod(degrees.x, 360);
+    if (m_rotation_angles.x < 0) {
+        m_rotation_angles.x += 360;
+    }
+    m_rotation_angles.y = fmod(degrees.y, 360);
+    if (m_rotation_angles.y < 0) {
+        m_rotation_angles.y += 360;
+    }
+    m_rotation_angles.z = fmod(degrees.z, 360);
+    if (m_rotation_angles.z < 0) {
+        m_rotation_angles.z += 360;
+    }
     m_needs_update = true;
 }
 
--- engine/math/Transformable.h
+++ engine/math/Transformable.h
@@ -7,7 +7,9 @@ namespace phi {
 class Transformable {
     mutable glm::mat4 m_transform;
     mutable bool m_needs_update;
-    glm::mat3 m_scale;
+    glm::vec3 m_rotation_angles;
+    glm::vec3 m_scale;
+    glm::vec3 m_position;
     glm::mat3 m_rotation;
 
 public:
@@ -15,7 +17,9 @@ public:
     virtual ~Transformable() {}
     virtual const glm::mat4 &GetTransform() const;
     virtual const glm::mat3 &GetRotation() const;
-    virtual glm::vec3 GetPosition() const;
+    virtual const glm::vec3 &GetRotationAngles() const;
+    virtual const glm::vec3 &GetScale() const;
+    virtual const glm::vec3 &GetPosition() const;
     virtual void SetScale(const glm::vec3 &s);
     virtual void SetPosition(const glm::vec3 &p);
     virtual void SetRotation(const glm::vec3 &degrees);
