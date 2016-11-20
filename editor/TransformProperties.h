1d25ff4660d6b1d1c085c354a3bdfecd7947f52d
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2016-11-20 17:40:09 +0100

    Added nice widget for manipulating object transformations

---
+++ editor/TransformProperties.h
@@ -0,0 +1,37 @@
+#ifndef PHI_EDITOR_TRANSFORM_PROPERTIES_H
+#define PHI_EDITOR_TRANSFORM_PROPERTIES_H
+#include <QGroupBox>
+
+#include "engine/scene/Entity.h"
+
+namespace Ui {
+class TransformProperties;
+}
+
+namespace phi {
+namespace editor {
+
+class TransformProperties : public QGroupBox {
+    Q_OBJECT
+
+    phi::Entity *m_entity;
+    bool m_supress_update;
+
+    Ui::TransformProperties *ui;
+
+public:
+    explicit TransformProperties(QWidget *parent = 0);
+    ~TransformProperties();
+
+private slots:
+    void OnTransformChanged(double);
+    void OnRotationEditFinished();
+
+public slots:
+    void OnEntitySelected(phi::Entity *);
+};
+
+}
+}
+
+#endif // PHI_EDITOR_TRANSFORM_PROPERTIES_H
