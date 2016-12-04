56cb2519ffbdc320dfb7d8308382dc15b7c22b59
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2016-12-04 09:59:25 +0100

    Minor cosmetic changes

--- assets/shaders/PhongMaterial.fs
+++ assets/shaders/PhongMaterial.fs
@@ -2,16 +2,20 @@
 
 GBUFFER_OUT_DECLARATION()
 
-#if !defined(ENABLE_DIFFUSE_TEXTURE)
-uniform vec4 Diffuse;
-#else
+#if defined(ENABLE_DIFFUSE_TEXTURE)
 layout(binding = 0) uniform sampler2D DiffuseTexture;
+#else
+uniform vec4 Diffuse;
 #endif
 
-#if !defined(ENABLE_SPECULAR_TEXTURE)
-uniform vec4 Specular;
-#else
+#if defined(ENABLE_SPECULAR_TEXTURE)
 layout(binding = 1) uniform sampler2D SpecularTexture;
+#else
+uniform vec4 Specular;
+#endif
+
+#if defined(ENABLE_NORMAL_MAPPING)
+layout(binding = 2) uniform sampler2D NormalTexture;
 #endif
 
 in vec3 Normal;
@@ -19,16 +23,21 @@ in vec4 Position;
 in vec2 UV;
 
 void main() {
-    GBUFFER_OUT_NORMAL(vec4(Normal, 1));
-#if !defined(ENABLE_DIFFUSE_TEXTURE)
-    GBUFFER_OUT_DIFFUSE(Diffuse);
+#if defined(ENABLE_NORMAL_MAPPING)
+    GBUFFER_OUT_NORMAL(texture(NormalTexture, UV));
 #else
-    GBUFFER_OUT_DIFFUSE(texture(DiffuseTexture, UV));
+    GBUFFER_OUT_NORMAL(vec4(Normal, 1));
 #endif
 
-#if !defined(ENABLE_SPECULAR_TEXTURE)
-    GBUFFER_OUT_SPECULAR(Specular);
+#if defined(ENABLE_DIFFUSE_TEXTURE)
+    GBUFFER_OUT_DIFFUSE(texture(DiffuseTexture, UV));
 #else
+    GBUFFER_OUT_DIFFUSE(Diffuse);
+#endif
+
+#if defined(ENABLE_SPECULAR_TEXTURE)
     GBUFFER_OUT_SPECULAR(texture(SpecularTexture, UV));
+#else
+    GBUFFER_OUT_SPECULAR(Specular);
 #endif
 }
