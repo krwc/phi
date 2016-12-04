abba4b069271f53b093693f17dfc7d6caa538912
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2016-12-04 12:26:21 +0100

    Added VERY naive light movement for tests

--- editor/PhiWidget.cpp
+++ editor/PhiWidget.cpp
@@ -8,6 +8,7 @@
 #include "engine/renderer/DebugDrawer.h"
 
 #include "engine/utils/MeshImporter.h"
+#include "engine/utils/TextureImporter.h"
 
 #include "engine/math/Ray.h"
 
@@ -37,12 +38,8 @@ static void SetupSampleScene(phi::Scene &scene) {
     wall_l->SetScale({4, 1, 8});
     wall_l->SetPosition({-30, 6, 0});
 
-    static phi::Texture2D random_texture(256, 256, phi::TextureFormat::RGB_888);
-    std::vector<uint8_t> noise(3 * 256 * 256);
-    for (unsigned i = 0; i < noise.size(); ++i) {
-        noise[i] = (rand() % 255);
-    }
-    random_texture.Write(0, 0, 0, 256, 256, noise.data());
+    static std::unique_ptr<phi::Texture2D> brick1 =
+            phi::TextureImporter::FromFile("assets/textures/brick1.jpg");
 
     torus->SetScale({4, 4, 4});
 
@@ -53,7 +50,7 @@ static void SetupSampleScene(phi::Scene &scene) {
     static auto pink_material = std::make_unique<phi::PhongMaterial>();
     pink_material->SetDiffuseColor({0.6, 0.2, 0.4, 1});
     static auto blue_material = std::make_unique<phi::PhongMaterial>();
-    blue_material->SetDiffuseTexture(random_texture);
+    blue_material->SetDiffuseTexture(*brick1.get());
     static auto violet_material = std::make_unique<phi::PhongMaterial>();
     violet_material->SetDiffuseColor({0.4, 0., 0.6, 1});
     static auto bunny_material = std::make_unique<phi::PhongMaterial>();
@@ -100,7 +97,8 @@ PhiWidget::PhiWidget(QWidget *widget)
         : QOpenGLWidget(widget),
           m_camera(std::make_unique<phi::FreeLookCamera>()),
           m_selected_entity(nullptr),
-          m_rotate(false) {
+          m_rotate(false),
+          m_rotate_light(false) {
     setFocusPolicy(Qt::StrongFocus);
 }
 
@@ -162,12 +160,16 @@ void PhiWidget::mousePressEvent(QMouseEvent *event) {
         emit EntityPicked(m_selected_entity);
     } else if (event->button() == Qt::RightButton) {
         m_rotate = true;
+    } else if (event->button() == Qt::MiddleButton) {
+        m_rotate_light = true;
     }
 }
 
 void PhiWidget::mouseReleaseEvent(QMouseEvent *event) {
     if (event->button() == Qt::RightButton) {
         m_rotate = false;
+    } else if (event->button() == Qt::MiddleButton) {
+        m_rotate_light = false;
     }
 }
 
@@ -201,9 +203,15 @@ void PhiWidget::handleInput() {
     int dy = m_mouse_position.y - y;
 
     if (m_rotate) {
-        // FIXME: this doesn't work well with rotations somehow
         m_camera->RotateX(-dy * 0.2);
         m_camera->RotateY(-dx * 0.2);
+    } else if (m_rotate_light) {
+        phi::DirLight *sun = *m_context->GetScene().GetDirLights().begin();
+        glm::vec3 P = sun->GetPosition();
+        glm::quat rotx = glm::rotate(glm::quat(), glm::radians(-dy * 0.2f), glm::vec3(1, 0, 0));
+        glm::quat roty = glm::rotate(glm::quat(), glm::radians(-dx * 0.2f), glm::vec3(0, 1, 0));
+        P = glm::mat3_cast(rotx * roty) * P;
+        sun->SetPosition(P);
     }
 
     m_mouse_position.x = x;
